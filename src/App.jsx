5812d07df5d5e9b17923aa6f2e78661728fbf22c
Dhanraj Jagtap <dhanraj.jagtap@atgeirsolutions.com>
2024-01-06 19:57:44 +0530

    Dhanraj : Folder structure and under construction page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import under_work from "./assets/construction.gif";
+function App() {
+  return (
+    <div className="App">
+      <img style={{ width: "98vw", height: "98vh" }} src={under_work} alt="" />
+    </div>
+  );
+}
+
+export default App;
